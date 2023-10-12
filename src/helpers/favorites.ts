ae52fa4383baa116c2b23657074779e774f11355
Kadir <mail.kadiryucel@gmail.com>
2023-10-12 22:34:41 +0300

    fix: favorite del func parameter is fix

--- src/helpers/favorites.ts
+++ src/helpers/favorites.ts
@@ -13,6 +13,6 @@ export function add(item:any){
     localStorage.setItem(key,JSON.stringify([item,...getFavorites()]))
 }
 
-export function del(id:number){
-    return localStorage.setItem(key,JSON.stringify(getFavorites().filter(x=>x.id !== id)))
+export function del(id:string){
+    localStorage.setItem(key,JSON.stringify(getFavorites().filter(x=>x.id !== id)))
 }
