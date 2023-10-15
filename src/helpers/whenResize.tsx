2864d9424b629379e33760035a13ba1ed9060c17
Kadir <mail.kadiryucel@gmail.com>
2023-10-15 19:44:12 +0300

    chore: remove console.log

--- src/helpers/whenResize.tsx
+++ src/helpers/whenResize.tsx
@@ -5,17 +5,14 @@ export default function useWhenResize(){
     const [currentWidth, setCurrentWidth] = useState<number>(0)
     useEffect(()=>{
         setCurrentWidth(window.innerWidth)
-        console.log(window.innerWidth)
         let debaounce = 500
         let currentBuffer: NodeJS.Timeout;
         function whenResize() {
-            console.log('hi')
             clearTimeout(currentBuffer)
             currentBuffer = setTimeout(() => {
                 let currentWidth = window.innerWidth;
 
                 setCurrentWidth(currentWidth)
-                console.log('Tarayıcı genişliği değişti. Yeni genişlik: ' + currentWidth);
             }, debaounce)
         }
 
