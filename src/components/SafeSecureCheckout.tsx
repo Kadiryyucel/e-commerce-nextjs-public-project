d8f311cfbe4e13ec2d58f3702f82885926d19c21
Kadir <mail.kadiryucel@gmail.com>
2024-01-01 16:35:35 +0300

    chore: added some config

--- src/components/SafeSecureCheckout.tsx
+++ src/components/SafeSecureCheckout.tsx
@@ -11,9 +11,9 @@ import oiCard from "../assets/oiCard.png";
 
 import { IoLockClosedOutline } from "react-icons/io5";
 
-export default function SafeSecureCheckout(props:{className?:string}) {
+export default function SafeSecureCheckout(props: { className?: string, showText?: boolean }) {
 
-    const { className } = props
+    const { className, showText = true } = props
 
     const supportedPay = [
         masterCard,
@@ -28,12 +28,12 @@ export default function SafeSecureCheckout(props:{className?:string}) {
         <>
             <div className={`flex flex-col justify-center gap-y-4 px-4 mt-8 h-32 bg-item ${className}`}>
                 <div className='flex justify-center items-center w-full'>
-                    <div><IoLockClosedOutline size={32}/></div>
-                    <div className='text-sm md:text-base xl:text-lg'>Guaranteed Safe & Secure Checkout</div>
+                    {showText && <div><IoLockClosedOutline size={32} /></div>}
+                    {showText && <div className='text-sm md:text-base xl:text-lg'>Guaranteed Safe & Secure Checkout</div>}
                 </div>
                 <div>
                     <div className='flex w-full gap-x-4 md:px-28 lg:px-10'>
-                        {supportedPay.map((x:StaticImageData,index:number) => {
+                        {supportedPay.map((x: StaticImageData, index: number) => {
                             return (
                                 <div key={index}><Image
                                     width={250}
